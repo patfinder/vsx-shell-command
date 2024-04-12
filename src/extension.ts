4909cd5a354648251c8c377133717a6f41861c6e
Vuong Nguyen <vuong.se@gmail.com>
2024-04-12 08:24:34 -0400

    Apply main code change

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,30 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('shell-command.execute', () => {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "shell-command" is now active!');
+		const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('shell-command.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Shell Command!');
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			// Get the word within the selection
+			const selectedText = document.getText(selection);
+
+			cp.exec(`echo "${selectedText}" | sort`, (_, output, _1) => {
+				console.log('Shell command output: ' + output);
+
+				// Only apply change if output not empty
+				output && editor.edit(editBuilder => {
+					editBuilder.replace(selection, output);
+				});
+			});
+			
+		}
 	});
 
 	context.subscriptions.push(disposable);
